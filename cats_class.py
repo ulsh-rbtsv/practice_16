ee4902f3a75937c0d9c8eba441990817e7f30d33
ulsh-rbtsv <70586183+ulsh-rbtsv@users.noreply.github.com>
2020-12-17 15:05:29 +0300

    var1: display func

---
+++ cats_class.py
@@ -0,0 +1,8 @@
+class Cats:
+    def __init__(self, name, sex, age):
+        self.name = name
+        self.sex = sex
+        self.age = age
+
+    def display_cats(self):
+        return self.name, self.sex, self.age
---
+++ return_cats.py
@@ -0,0 +1,8 @@
+from cats_class import Cats
+cat_1 = Cats('Baron', 'male', 2)
+cat_2 = Cats('Sam', 'male', 2)
+
+print(cat_1.display_cats())
+print(cat_2.display_cats())
+
+

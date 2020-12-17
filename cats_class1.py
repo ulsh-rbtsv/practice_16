32c13dfbb44d1b4785566f1b75c62a4b76052a8a
ulsh-rbtsv <70586183+ulsh-rbtsv@users.noreply.github.com>
2020-12-17 15:07:00 +0300

    var2: print func

---
+++ cats_class1.py
@@ -0,0 +1,5 @@
+class Cats:
+    def __init__(self, name, sex, age):
+        self.name = name
+        self.sex = sex
+        self.age = age
---
+++ print_cats.py
@@ -0,0 +1,8 @@
+from cats_class import Cats
+cat_1 = Cats('Baron', 'male', 2)
+cat_2 = Cats('Sam', 'male', 2)
+
+print('name1: ', cat_1.name, 'sex1: ', cat_1.sex, 'age1: ', cat_1.age)
+print('name2: ', cat_2.name, 'sex2: ', cat_2.sex, 'age2: ', cat_2.age)
+
+
